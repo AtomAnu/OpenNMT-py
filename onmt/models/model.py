e2c373cc29ccf969c4f96067cc1c381c1ee674f5
AtomAnu <a.anuchitanukul@gmail.com>
2021-06-23 21:38:40 +0700

    Update model.py

--- onmt/models/model.py
+++ onmt/models/model.py
@@ -61,6 +61,8 @@ class NMTModel(BaseModel):
     def forward(self, src, tgt, lengths, bptt=False, with_align=False):
         dec_in = tgt[:-1]  # exclude last target from inputs
 
+        print('dec_in: {}'.shape(dec_in.shape))
+
         enc_state, memory_bank, lengths = self.encoder(src, lengths)
 
         if not bptt:
