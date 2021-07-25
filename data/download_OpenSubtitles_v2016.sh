cb8188cb45773300530e9017f35c3f12a54026ad
AtomAnu <a.anuchitanukul@gmail.com>
2021-07-25 14:55:17 +0700

    Updated OpenSubtitles dataset download script

--- data/download_OpenSubtitles_v2016.sh
+++ data/download_OpenSubtitles_v2016.sh
@@ -5,5 +5,5 @@
 # OpenSubtitles v2016
 ##################################################################################
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Rra4XSEsI8e6WXcPL56SDJ7qsI-hlrUn' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Rra4XSEsI8e6WXcPL56SDJ7qsI-hlrUn" -O OpenSubtitles-v2016.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1LGzSA_kz8WhIqFXyRKcjkdtBPvIAlb_K' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1LGzSA_kz8WhIqFXyRKcjkdtBPvIAlb_K" -O OpenSubtitles-v2016.zip && rm -rf /tmp/cookies.txt
 unzip OpenSubtitles-v2016.zip
