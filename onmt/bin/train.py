91dc963d1bd9cc3cb3d2d6b5d9c4cc371d89c041
AtomAnu <a.anuchitanukul@gmail.com>
2021-08-06 00:02:11 +0700

    Experimenting with process-wise reward computation

--- onmt/bin/train.py
+++ onmt/bin/train.py
@@ -116,7 +116,7 @@ def train(opt):
         global_gpu_id = opt.gpu_ranks[-1] + 1
 
         # Build a global A2C model.
-        global_a2c = build_model(model_opt, opt, fields, checkpoint)
+        global_a2c = build_model(model_opt, opt, fields, checkpoint, gpu_id=global_gpu_id)
         global_a2c.share_memory()
         # global_a2c.to('cuda:'+ str(global_gpu_id))
 
