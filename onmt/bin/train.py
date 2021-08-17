bf1beb21cf9ffa4ca203ec47d9b3b28195650747
AtomAnu <a.anuchitanukul@gmail.com>
2021-08-17 16:21:41 +0700

    Code optimization

--- onmt/bin/train.py
+++ onmt/bin/train.py
@@ -111,7 +111,7 @@ def train(opt):
 
     if opt.async or opt.model_task == ModelTask.A3C and opt.train_mode in [TrainMode.CRITIC, TrainMode.AC]:
 
-        print("Actor LR ***".format(opt.actor_learning_rate))
+        print("Actor LR *** {}".format(opt.actor_learning_rate))
 
         model_opt = _get_model_opts(opt, checkpoint=checkpoint)
 
