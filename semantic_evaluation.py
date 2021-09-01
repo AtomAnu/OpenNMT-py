c544542c959b4eb840ecd55003bdc555f37edcb2
AtomAnu <a.anuchitanukul@gmail.com>
2021-09-01 13:08:13 +0700

    Added Semantic-level Evaluation

--- semantic_evaluation.py
+++ semantic_evaluation.py
@@ -64,9 +64,9 @@ class SemanticEval():
         # print('3/3')
         slss_scores = self._compute_sentence_level_semantic_similarity(src_list, hyp_list)
 
-        avg_exp_fluency = (fluency_scores + 5).exp().mean()
-        avg_exp_tlss = (tlss_scores + 5).exp().mean()
-        avg_exp_slss = (slss_scores + 5).exp().mean()
+        avg_exp_fluency = (fluency_scores).exp().mean()
+        avg_exp_tlss = (tlss_scores).exp().mean()
+        avg_exp_slss = (slss_scores).exp().mean()
 
         return avg_exp_fluency, avg_exp_tlss, avg_exp_slss
 
